a1529b51e8a74df40ab55f947d0224b663e1151a
srijangupta-work <srijangupta.work@gmail.com>
2026-01-08 11:51:44 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,17 @@
+# Perform Basic Mathematical Operations
+
+x = float(input("Enter The First Number : "))
+y = float(input("Enter The Second Number : "))
+
+addition = x+y
+subtraction = x-y
+multiplication = x*y
+division = x/y
+
+print()
+print()
+
+print("Addition : " , addition)
+print("Subtraction : " , subtraction)
+print("Multiplication : " , multiplication)
+print("Division : " , division)
---
+++ Task2.py
@@ -0,0 +1,11 @@
+# Create a Personalized Greeting
+
+first_name = str(input("Enter Your First Name : "))
+
+last_name = str(input("Enter Your Second Name : "))
+
+full_name = first_name +" "+last_name
+
+print()
+
+print("Hello," ,full_name , "! Nice To Meet You.")
